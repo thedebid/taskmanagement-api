476851cd08e4c06047c82f74e891ee94e608cdaf
Debid Magar <linkwithdm@gmail.com>
2020-07-10 22:26:55 +0545

    task created

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { v4 as uuidv4 } from 'uuid';
+import { Task, TaskStatus } from './task.model';
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
   getAllTasks() {
     return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
