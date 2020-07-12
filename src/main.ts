319f3538c686bca93df241601ee4640c48ef55e4
Debid Magar <linkwithdm@gmail.com>
2020-07-12 19:14:57 +0545

    swagger ui added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('The task management API description')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   console.log('Server Started');
 }
